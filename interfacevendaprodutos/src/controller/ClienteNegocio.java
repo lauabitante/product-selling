1cc0f64b4cc1ef92bbba1f57a050484b3bb9d043
Laura Abitante <laura.abitante@gmail.com>
2018-07-11 23:22:48 -0300

    Refatoração da classe ClienteNegocio.java

--- interfacevendaprodutos/src/controller/ClienteNegocio.java
+++ interfacevendaprodutos/src/controller/ClienteNegocio.java
@@ -20,119 +20,14 @@ import model.Conta;
  */
 public class ClienteNegocio {
 
-    private DecimalFormat df = new DecimalFormat("0.##");    
     private ClienteDAO clienteDAO = new ClienteDAO_BD();
-
-    /**
-     * Cadastra um cliente a partir do input do usuário
-     */
-    public void cadastrarCliente() {
-        String nomeCliente = this.solicitarDadoString("\nNome: ", false);
-        
-        String cpfCliente;
-        do {
-            cpfCliente = this.solicitarDadoString("CPF: ", false);
-        } while (verificarCPFexistente(cpfCliente) == true);
-        
-        String emailCliente = this.solicitarDadoString("Email: ", false);
-        int numeroContaCliente = this.solicitarDadoInt("Conta: ");
-        Double saldoContaCliente = this.solicitarDadoDouble("Saldo: ");
-
-        Conta conta = new Conta(numeroContaCliente, saldoContaCliente);
-        Cliente cliente = new Cliente(nomeCliente, emailCliente, cpfCliente, conta);
-        salvarCliente(cliente);
-    }
     
     public void salvarCliente(Cliente cliente) {
         clienteDAO.salvar(cliente);
     }
-
-    public String solicitarDadoString(String mensagem, boolean aceitaNull) {
-        Scanner scan = new Scanner(System.in);
-        String dado;
-        do {
-            System.out.print(mensagem);
-            dado = scan.nextLine().trim();
-        } while (dado.length() == 0 && aceitaNull == false);
-
-        return dado;
-    }
-
-    public int solicitarDadoInt(String mensagem) {
-        Scanner scan = new Scanner(System.in);
-        int dado = 0;
-        do {
-            try {
-                System.out.print(mensagem);
-                dado = scan.nextInt();
-            } catch (InputMismatchException ex) {
-                System.out.println("Valor inválido.");
-                scan.next();
-            }
-        } while (dado == 0);
-
-        return dado;
-    }
-    
-    public Double solicitarDadoDouble(String mensagem) {
-        Scanner scan = new Scanner(System.in);
-        Double dado = 999999.99;
-        do {
-            try {
-                System.out.print(mensagem);
-                dado = scan.nextDouble();
-            } catch (NumberFormatException ex) {
-                System.out.println("Valor inválido.");
-                scan.next();
-            }
-        } while (dado == 999999.99);
-
-        return dado;
-    }
-
-    /**
-     * Lista clientes cadastrados
-     */
-    public void listarClientesAtivos() {
-        System.out.println("\n");
-        List<Cliente> clientes = clienteDAO.listar(true);
-        if (clientes.isEmpty()) {
-            separador();
-            System.out.println("NENHUM CLIENTE CADASTRADO");
-        } else {
-            System.out.println("");
-            separador();
-            System.out.println("LISTA DE CLIENTES");
-            separador();
-            for (int i = 0; i < clientes.size(); i++) {
-                Cliente cliente = clientes.get(i);
-                System.out.println(cliente.toString());
-            }
-        }
-        separador();
-    }
     
     public List<Cliente> listaClientesAtivos(boolean ativo) {
         return clienteDAO.listar(ativo);
-    }
-    
-    public void atualizarDadosCliente() {
-        Cliente cliente = getCliente();
-        System.out.println("Preencha os campos que deseja alterar. Pressione enter caso não tenha alteração.");
-        String nome = this.solicitarDadoString("Nome: ", true);
-        String email = this.solicitarDadoString("Email: " , true);
-        String cpf = this.solicitarDadoString("CPF: " , true);
-        
-        if (!nome.isEmpty()) {
-            cliente.setNome(nome);
-        }
-        if (!email.isEmpty()) {
-            cliente.setEmail(email);
-        }
-        if (!cpf.isEmpty()) {
-            cliente.setCpf(cpf);
-        }
-        clienteDAO.atualizar(cliente);
     }
     
     public void atualizarDadosCliente(Cliente cliente) {
@@ -141,7 +36,6 @@ public class ClienteNegocio {
      
      public void atualizarSaldoCliente(Cliente cliente) {
          clienteDAO.atualizar(cliente);
-         
      }
     
     public void deletarCliente() {
@@ -193,7 +87,6 @@ public class ClienteNegocio {
             c.getConta().depositar(valor);
             clienteDAO.atualizar(c);
             Double saldoAtual = c.getConta().getSaldoConta();
-            System.out.println("\nDepósito realizado com sucesso! Saldo Atual: " + df.format(saldoAtual));
         } else {
             System.out.println("\nValor inválido.");
         }
@@ -247,7 +140,6 @@ public class ClienteNegocio {
     public void consultarSaldo() {
         Cliente c = getCliente();
         if (c != null) {
-            System.out.println("Saldo: " + df.format(c.getConta().getSaldoConta()));
         }
     }
 
@@ -259,7 +151,7 @@ public class ClienteNegocio {
     public Cliente getCliente() {
         Scanner scan = new Scanner(System.in);
         List<Cliente> clientesAtivos = clienteDAO.listar(true);
-        listarClientesAtivos();
+//        listarClientesAtivos();
         if (!clientesAtivos.isEmpty()) {
             System.out.print("\nDigite o código do cliente: ");
             int codigo = scan.nextInt();
